698275fa25e932e4e7609a0779c913ddfe0f959e
Jaewan <chlcjw3@naver.com>
2019-11-30 01:27:56 +0900

    MedicalStateTeamProject

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,46 @@
-import MainView from './views/MainView';
+import MedicalState from './views/MedicalState';
+import DoctorAddPatientView from './views/DoctorAddPatient';
+import DoctorLogin from './views/DoctorLogin';
+import DoctorPrescription from './views/DoctorPrescription';
+import DoctorSearch from './views/DoctorSearch';
+import DoctorSignUp from './views/DoctorSignUp';
 
 const routes = [
   {
     id: 'main',
     path: '/',
     exact: true,
-    component: MainView,
+    component: MedicalState,
+  },
+  {
+    id: 'DoctorLogin',
+    path: '/doctor/login',
+    exact: true,
+    component: DoctorLogin,
+  },
+  {
+    id: 'DoctorSignUp',
+    path: '/doctor/signup',
+    exact: true,
+    component: DoctorSignUp,
+  },
+  {
+    id: 'DoctorSearch',
+    path: '/doctor/search',
+    exact: true,
+    component: DoctorSearch,
+  },
+  {
+    id: 'DoctorAddPatientView',
+    path: '/doctor/addpatientview',
+    exact: true,
+    component: DoctorAddPatientView,
+  },
+  {
+    id: 'DoctorPrescription',
+    path: '/doctor/prescription',
+    exact: true,
+    component: DoctorPrescription,
   },
 ];
 
