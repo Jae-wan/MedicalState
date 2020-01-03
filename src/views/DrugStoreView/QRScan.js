9af83451e5e2547f6c5a22c28412acac1dfaeb4b
Term Sigening <37768891+Sigening@users.noreply.github.com>
2020-01-03 21:39:46 +0900

    Add files via upload

--- src/views/DrugStoreView/QRScan.js
+++ src/views/DrugStoreView/QRScan.js
@@ -1,18 +1,43 @@
 //QR스캔 및 환자정보 보여주기
 
 import React, { Component } from 'react';
-import Link from '../common/Link';
+import QrReader from 'react-qr-reader';
+import QRCode from 'qrcode.react';
+// qrcode.react -> QR코드 생성하는 패키지, 사용법: <QRCode value={"주소"} />
 
-class QRScan extends Component {
-
-    render(){
-        return(
-            <div>
-                <Link />
-                <h1>QR스캔 페이지.</h1>
-            </div>
-        )
+class QRReader extends Component {
+  
+    // 이미지 스캔 결과물 (data는 주소)
+    handleScan = data => {
+      console.log(data);
     }
-}
 
-export default QRScan;
+    // 에러 처리 함수 (관상용)
+    handleError = err => {
+      console.error(err)
+    }
+
+    // 이미지 로드 버튼
+    openImageDialog = () => {
+      this.refs.reader.openImageDialog();
+    }
+
+  render() {
+    return (
+      <div>
+	    <button onClick={this.openImageDialog}> Insert QRCode </button>
+
+        <QrReader
+          onError={this.handleError}
+          onScan={this.handleScan}
+		  legacyMode={true}
+		  ref="reader"
+          style={{ width: '0%' }} 
+        />
+
+        <QRCode value={"http://naver.com/"} />
+      </div>
+    )
+  }
+}
+export default QRReader;
